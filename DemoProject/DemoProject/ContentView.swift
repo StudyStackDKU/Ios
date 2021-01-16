c8e3e6587419ae3556e4b1d32c455d5a188c5fa9
yuna <cya3559@naver.com>
2021-01-17 08:47:25 +0900

    책 자습: 스위프트UI 프로젝트 생성

---
+++ DemoProject/DemoProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoProject
+//
+//  Created by 정윤아 on 2021/01/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoProject/DemoProject/DemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjectApp.swift
+//  DemoProject
+//
+//  Created by 정윤아 on 2021/01/16.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
